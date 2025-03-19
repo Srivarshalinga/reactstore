a78bf98c77d4d11752d85e67a13bb1d981a01785
srivarshalinga <srivarshalinga@gmail.com>
2025-03-19 10:21:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Products />
+      <Footer/>
     </div>
   );
 }
-
 export default App;
+
+
+
+// import './App.css';
+// import Header from './Header';
+// import Main from './Main';
+// import Footer from './Footer';
+// import Login from './Login';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header/>
+//       <Main/>
+//       <Footer/>
+//     </div>
+  
+//   );
+// }
+
+// export default App;
